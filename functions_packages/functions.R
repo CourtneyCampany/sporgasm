e1f7d48a7c29561d12ae2313f97a0ea18f3a2d26
Courtney Cammpany <courtneycampany@gmail.com>
2017-07-05 11:59:05 -0600

    added new raw data files

---
+++ functions_packages/functions.R
@@ -0,0 +1,12 @@
+
+# functions for sporgasm project ------------------------------------------
+
+###Here are the various function used in the sporsgasm project
+
+
+# pressure volume curves --------------------------------------------------
+
+pv_curve_func <- functions(x){
+  
+  
+}
---
+++ master_scripts/sporophyte_plc.R
@@ -0,0 +1,5 @@
+
+# description -------------------------------------------------------------
+
+###This script will load and analyze plc curves for fern sprophytes
+library(fitplc)
