a2402e377a53851e260ea953339b50e8f9e5dddc
Courtney Cammpany <courtneycampany@gmail.com>
2017-07-06 18:17:29 -0600

    more data entry for day 2

--- gametophyte_drydown.R
+++ gametophyte_drydown.R
@@ -7,12 +7,21 @@ drydown <- read.csv("raw_data/gam_drydown_trial.csv")
 
 
 # data formatting ---------------------------------------------------------
-drydown$date_t0 <- as.Date(drydown$date_t0, format = "%d/%m/%Y")
-drydown$date_t1 <- as.Date(drydown$date_t1, format = "%d/%m/%Y")
+drydown$date_t0 <- as.Date(drydown$date_t0, format = "%m/%d/%Y",tz='UTC')
+drydown$date_t1 <- as.Date(drydown$date_t1, format = "%m/%d/%Y",tz='UTC')
+drydown$date_t2 <- as.Date(drydown$date_t2, format = "%m/%d/%Y",tz='UTC')
+drydown$date_t2 <- as.Date(drydown$date_t3, format = "%m/%d/%Y",tz='UTC')
 drydown$id <- as.factor(drydown$id)
 
+#calculate gametophyte mass
 gam_mass_init <- with(drydown, total_mass_t0-filter_mass)
 gam_mass_t1 <- with(drydown, total_mass_t1-filter_mass)
+gam_mass_t2 <- with(drydown, total_mass_t2-filter_mass)
+gam_mass_t3 <- with(drydown, total_mass_t3-filter_mass)
+
+#merge date and time into datetime object
+drydown$datetime <- with(drydown, paste(date_t0, time0, sep = " "))
+drydown$datetime2 <- as.POSIXct(drydown$datetime,tz='UTC', format="%Y-%m-%d %H:%M")
 
 # plot objects ------------------------------------------------------------
 chems <- c("Control", "KNo3", "MgSo4", "NaCl")
@@ -20,6 +29,11 @@ cols <- c("forestgreen", "cornflowerblue", "gold", "firebrick4")
 
 
 # plotting ----------------------------------------------------------------
+
+lomves <- drydown[drydown$species=="lomves",]
+camp <- drydown[drydown$species=="camp",]
+
+plot()
 
 ###percent mass loss
 windows()
