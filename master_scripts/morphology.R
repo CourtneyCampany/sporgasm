b7eea2def39da7852b07b73526cfcf74d7c4d3de
Courtney Campany <courtneycampany@gmail.com>
2019-01-23 08:57:39 -0500

    morphology stats

--- master_scripts/morphology.R
+++ master_scripts/morphology.R
@@ -2,7 +2,9 @@ source("master_scripts/plot_objects.R")
 
 traits <- read.csv("calculated_data/fern_traits.csv")
 
+windows()
 
+par(mar=c(5,5,1,1), mfrow=c(3,1))
 boxplot(lamina_area_cm2 ~ niche, data=traits, ylim=c(0, 1750),xaxt='n',
         ylab=lamina_lab, outline=FALSE)
 axis(1, niche_lab, at=1:4, cex=1.1)
@@ -15,7 +17,7 @@ axis(1, niche_lab, at=1:4, cex=1.1)
 text(x=c(1,2,3,4),y=170, labels=niche_lab2)
 
 
-boxplot(stipe_length_cm ~ niche, data=traits, ylim=c(0, 177),xaxt='n',
+boxplot(stipe_length_cm ~ niche, data=traits, ylim=c(0, 80),xaxt='n',
         ylab = stipe_lab,outline=FALSE)
 axis(1, niche_lab, at=1:4, cex=1.1)
-text(x=c(1,2,3,4),y=170, labels=niche_lab2)
+text(x=c(1,2,3,4),y=75, labels=niche_lab2)
