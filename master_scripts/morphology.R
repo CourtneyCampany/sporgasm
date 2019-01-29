f5c2eb89a7b8083a0934e0fd4fd289cd163300e7
Courtney Campany <courtneycampany@gmail.com>
2019-01-29 17:25:16 -0500

    las cruces vcruce data clean

--- master_scripts/morphology.R
+++ master_scripts/morphology.R
@@ -2,13 +2,11 @@ source("master_scripts/plot_objects.R")
 
 traits <- read.csv("calculated_data/fern_traits.csv")
 
-<<<<<<< HEAD
 windows()
 
 par(mar=c(5,5,1,1), mfrow=c(3,1))
-=======
+
 # first plots------
->>>>>>> 59ab7caa917d2aaab458559fc12eebc56710acdc
 boxplot(lamina_area_cm2 ~ niche, data=traits, ylim=c(0, 1750),xaxt='n',
         ylab=lamina_lab, outline=FALSE)
 axis(1, niche_lab, at=1:4, cex=1.1)
@@ -24,10 +22,9 @@ text(x=c(1,2,3,4),y=170, labels=niche_lab2)
 boxplot(stipe_length_cm ~ niche, data=traits, ylim=c(0, 80),xaxt='n',
         ylab = stipe_lab,outline=FALSE)
 axis(1, niche_lab, at=1:4, cex=1.1)
-<<<<<<< HEAD
+
 text(x=c(1,2,3,4),y=75, labels=niche_lab2)
-=======
-text(x=c(1,2,3,4),y=170, labels=niche_lab2)
+
 
 ## Climber seems to be close to terrestrial and it still is technically-------
 ## create new variable that adds climber to terrestrial category
@@ -40,24 +37,45 @@ traits$niche2 <- traits$niche
   no_epi <- length(unique(traits[traits$niche =="epiphyte", "species"]))
   no_hemi <- length(unique(traits[traits$niche =="hemi-epiphyte", "species"]))
   
-niche_count2 <- c(no_epi, no_hemi, no_terr)
-  niche_lab3 <- paste("n=", niche_count, sep = "")
+niche_count2 <- c(no_terr, no_hemi, no_epi)
+  niche_lab <- paste("n=", niche_count2, sep = "")
   
 #boxplots-----  
-boxplot(lamina_area_cm2 ~ niche2, data=traits, ylim=c(0, 2000),xaxt='n',
-          ylab=lamina_lab, outline=FALSE)
-  axis(1, niche_lab_noclimb, at=1:3, cex=1.1)
-  text(x=c(1,2,3,4),y=1900, labels=niche_lab3)
+  gradient <- colorRampPalette(c("forestgreen","steelblue4"))
+  palette(gradient(3))
+  trtcols <- palette(gradient(3))
   
+#reorder from ground to canopy 
+  traits$niche2<-factor(traits$niche2, 
+               levels=c("terrestrial", "hemi-epiphyte", "epiphyte"))
+  
+boxlabs <- c("Terrestrial", "Hemi-epiphyte", "Epiphyte")
+  
+
+jpeg(filename = "jobtalk/leafsize.jpeg",
+     width = 7, height = 7, units = "in", res= 400)
+
+par(las=1, cex.axis=1, cex.lab=1.25, mgp=c(3,1,0), mar=c(4,5,1,1))
+
+boxplot(lamina_area_cm2 ~ niche2, data=traits, ylim=c(0, 2000),xaxt='n',
+          ylab=lamina_lab, outline=FALSE, col=trtcols)
+  axis(1, boxlabs, at=1:3, cex=1.1,cex.axis=1.25)
+  text(x=c(1,2,3),y=1900, labels=niche_lab)
+
+dev.off()
   
 boxplot(frond_length_cm ~ niche2, data=traits, ylim=c(0, 200),xaxt='n',
           ylab = frond_lab,outline=FALSE)
   axis(1, niche_lab_noclimb, at=1:3, cex=1.1)
   text(x=c(1,2,3,4),y=190, labels=niche_lab3)
   
-  
-boxplot(stipe_length_cm ~ niche2, data=traits, ylim=c(0, 95),xaxt='n',
-          ylab = stipe_lab,outline=FALSE)
-  axis(1, niche_lab_noclimb, at=1:3, cex=1.1)
-  text(x=c(1,2,3,4),y=85, labels=niche_lab3)
->>>>>>> 59ab7caa917d2aaab458559fc12eebc56710acdc
+
+jpeg(filename = "jobtalk/stipe.jpeg",
+       width = 7, height = 7, units = "in", res= 400)  
+
+par(las=1, cex.axis=1, cex.lab=1.25, mgp=c(3,1,0), mar=c(4,5,1,1))
+boxplot(stipe_length_cm ~ niche2, data=traits, ylim=c(0, 75),xaxt='n',
+          ylab = stipe_lab,outline=FALSE, col=trtcols)
+axis(1, boxlabs, at=1:3, cex=1.1,cex.axis=1.25)
+text(x=c(1,2,3),y=70, labels=niche_lab)
+dev.off()
