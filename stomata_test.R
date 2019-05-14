c82492cdddacaa72576e4659e5db9228937a3498
Courtney Cammpany <courtneycampany@gmail.com>
2019-05-14 08:43:52 -0400

    testing repo

--- stomata_test.R
+++ stomata_test.R
@@ -47,6 +47,10 @@ trtcols2 <- c(alpha(trtcols[1], .7), alpha(trtcols[2], .7),alpha(trtcols[3], .7)
 
 
 windows()
+
+jpeg(filename = "output/stomata_regression.jpeg",
+     width = 10, height = 7, units = "in", res= 400)
 plot(stomatal_size ~ sd_mm2, data=stomata, col=niche2, pch=16)
+dev.off()
 
                  
