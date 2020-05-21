2ef3b09c73c59448b87374358fc676ef1e41e835
Courtney Campany <CCAMPANY@shepherd.edu>
2020-05-21 15:54:07 -0400

    draft manuscript

--- stats/stomata_regression.R
+++ stats/stomata_regression.R
@@ -19,23 +19,34 @@ hemi <- alldata[alldata$niche2 == "hemi-epiphyte" ,]
 epi <- alldata[alldata$niche2 == "epiphyte",]
 
 #bivariate mixed model
-sizedens <- lmer(log10(sd_mm2) ~ log10(stomatal_size)  * niche2 
+sizedens <- lmer(log10(sd_mm2) ~ log10(stomatal_size * 1000)  * niche2 
                  + (1|species),  data=alldata)
 
-sizedens2 <- lm(sd_mm2 ~ stomatal_size  * niche2 ,  data=alldata)
-Anova(sizedens2, type=3)
-summary(sizedens2) #r2=.41, p < .00001
+sizedens2 <- lmer(log10(sd_mm2) ~ log10(stomatal_size * 1000)  + niche2 
+                 + (1|species),  data=alldata)
+
+sizedens3 <- lmer(sqrt(sd_mm2) ~ sqrt(stomatal_size*1000)  * niche2 
+                 + (1|species),  data=alldata)
+
+
+#model diagnostics
+qqPlot(residuals(sizedens3)) 
+plot(sizedens3)
+
+Anova(sizedens, type=3) #p = .035
+anova(sizedens, sizedens2)
+AIC(sizedens, sizedens2)
+
+summary(sizedens)
+r.squaredGLMM(sizedens)
+#R2m       R2c
+#0.2232516 0.9032065
 
 anova(lm(sd_mm2 ~ stomatal_size ,  data=terr))
 anova(lm(sd_mm2 ~ stomatal_size ,  data=hemi))
 anova(lm(sd_mm2 ~ stomatal_size ,  data=epi))
 
-#model diagnostics
-par(mar=c(4,4,1,1))
-qqPlot(residuals(sizedens2)) 
-plot(sizedens2)
-
-
+#test slopes and elevations
 sizedens3 <- sma(sd_mm2 ~ stomatal_size * niche2,
                     data=alldata, multcomp = TRUE,
                     multcompmethod='adjusted') #slopes not equal
